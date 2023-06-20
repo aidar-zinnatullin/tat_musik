8a9e1936880f5d2c570779497655e0eb8da713c4
aidar-zinnatullin <aidar.zinnatulla@gmail.com>
2023-06-20 20:45:50 +0400

    More updates

--- TextAnalysis.R
+++ TextAnalysis.R
@@ -1,4 +1,6 @@
 ##### Text analysis
 load(here("data", "Songs_Text.RData"))
-write.csv()
+names(more_info_2)[1] <- "text"
+write.csv(more_info_2, file = "to_stemm.csv")
 ## But before, stem / lemmatize on Python
+stemmed <-  read_csv("for_to_stemm.csv")
